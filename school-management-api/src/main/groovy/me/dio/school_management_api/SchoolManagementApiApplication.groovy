7f19a9c4ec8ce4f928a5cf6a18cf30831f8d9981
ronaldofelix <ronaldobboyh16@gmail.com>
2024-08-18 22:19:36 +0000

    commit test

---
+++ school-management-api/src/main/groovy/me/dio/school_management_api/SchoolManagementApiApplication.groovy
@@ -0,0 +1,13 @@
+package me.dio.school_management_api
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+public class SchoolManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementApiApplication, args)
+	}
+
+}
